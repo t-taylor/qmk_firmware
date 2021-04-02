3e767ece518fe07b3bd11ae76ed4d44eab62cab8
Tom Taylor <tom@planccck.xyz>
2021-04-02 19:38:16 +0100

    keymaps

--- keyboards/kbdfans/maja/keymaps/tom/keymap.c
+++ keyboards/kbdfans/maja/keymaps/tom/keymap.c
@@ -8,9 +8,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		    KC_LSFT,       KC_Z,     KC_X,     KC_C,  KC_V,  KC_B,  KC_GRV,  KC_N,  KC_M,    KC_COMM,  KC_DOT,   KC_SLSH,  KC_RSFT, KC_UP,
 		    KC_LGUI,                 KC_LALT,  MO(1),        KC_BSPC,        RGUI_T(KC_SPC), KC_LALT,                      KC_LEFT, KC_DOWN,   KC_RIGHT),
 		[1] = LAYOUT( /* FN */
-			KC_TRNS,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  RGB_TOG, KC_HOME,
+			KC_TRNS,       KC_F1,    KC_F2,   KC_F3,  KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  RGB_TOG, KC_MSEL,
 		    KC_TRNS,       KC_TRNS, KC_TRNS,  KC_PGUP,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_PSCR,  KC_TRNS, KC_TRNS, RESET,   KC_PGUP,
-			KC_CAPS,       KC_TRNS,  KC_HOME, KC_PGDN,KC_END,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,  KC_TRNS,          EEP_RST, KC_PGDN,
-		    KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS,
-		    KC_TRNS,                 KC_TRNS, KC_TRNS,         KC_TRNS,           KC_TRNS,         KC_TRNS,                    KC_TRNS, KC_TRNS, KC_TRNS),
+			KC_CAPS,       KC_TRNS,  KC_HOME, KC_PGDN,KC_END,  KC_TRNS, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_TRNS,  KC_TRNS,          EEP_RST, KC_MPLY,
+		    KC_TRNS,       KC_TRNS,  KC_TRNS, KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS, KC_VOLU,
+		    KC_TRNS,                 KC_TRNS, KC_TRNS,         KC_TRNS,           KC_TRNS,         KC_TRNS,                    KC_MPRV, KC_VOLD, KC_MNXT),
  };
--- keyboards/kyria/keymaps/default/keymap.c
+++ keyboards/kyria/keymaps/default/keymap.c
@@ -28,7 +28,7 @@ enum layers {
     _FUNCTION
 };
 
-#define FUN OSL(_FUNCTION)
+#define FUN MO(_FUNCTION)
 #define DELRAI LT(_RAISE, KC_DEL)
 #define CTL_ESC CTL_T(KC_ESC)
 #define OSM_ALT OSM(MOD_LALT)
@@ -57,7 +57,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       KC_GRV,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,                                         KC_Y,    KC_U,  KC_I,    KC_O,    KC_P,    KC_MINS,
       KC_LSFT, KC_A,   KC_S,   KC_D,   KC_F,   KC_G,                                         KC_H,    KC_J,  KC_K,    KC_L,    KC_SCLN, SHT_QUO,
       OSM_ALT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,   KC_EQL,  KC_CAPS,  KC_MENU,  KC_BSLS, KC_N,    KC_M,  KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                               KC_APP, FUN,    DELRAI, KC_BSPC, CTL_ESC,  ENT_LOW,  SPC_GUI, RAI_TAB, FUN,   _______
+                               KC_APP, FUN,    DELRAI, KC_BSPC, CTL_ESC,  ENT_LOW,  SPC_GUI, RAI_TAB, FUN,   KC_MPLY
     ),
 /*
  * Lower Layer: Symbols
